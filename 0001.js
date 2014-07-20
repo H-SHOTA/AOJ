08b35580114a4d6dd9e2adca3244a44849a5ccc5
a1148sh <a1148sh@aiit.ac.jp>
2014-07-20 12:42:46 +0900

    ・ファイル名変更 ・回答追加

--- 0001.js
+++ 0001.js
@@ -1,12 +1,31 @@
-/**
- * 
- */
-function ans(){
-	for(var i = 1; i <= 9; i++ ){
-		for(var ii = 1; ii <= 9; ii++ ){
-			console.log(i + "x" + ii + "=" + (i*ii));
+
+ 
+var input = "";
+
+function main(data){
+	for(var i = 0; i < data.length - 1; i++){
+		for(var j = 1; j < data.length; j++){
+			if(data[j - 1] < data[j]){
+				var tmp = data[j];
+				data[j] = data[j - 1];
+				data[j - 1] = tmp;
+			}
 		}
 	}
+	console.log(data[0]);
+	console.log(data[1]);
+	console.log(data[2]);
 }
 
-ans();
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(chunk) {
+  input += chunk;
+});
+process.stdin.on('end', function() {
+  input = input.split('\n');
+  for(var i = 0; i < input.length; i++){
+	  input[i] = parseInt(input[i])
+  }
+  main(input);
+});
---
+++ 0002.js
@@ -0,0 +1,23 @@
+var input = "";
+
+function main(data){
+	for(var i = 0; i < data.length; i++) {
+		var d = data[i].split(' ');
+		if(d.length == 2){
+			console.log(String(parseInt(d[0]) + parseInt(d[1])).length);
+		}
+		else{
+			break;
+		}
+	}
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(chunk) {
+  input += chunk;
+});
+process.stdin.on('end', function() {
+	input = input.split('\n');
+	main(input);
+});
